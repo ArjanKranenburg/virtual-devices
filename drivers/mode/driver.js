fe14a9e801be93f7b5e822a39ce1309d68dd5ca0
Arjan Kranenburg <Arjan.Kranenburg@gmail.com>
2017-07-07 14:28:56 +0200

    Allowed to select icon for mode

--- drivers/mode/driver.js
+++ drivers/mode/driver.js
@@ -28,66 +28,6 @@ const config = {
 	}
 };
 
-const name = {
-	mode: {
-		en: "Mode",
-		nl: "Modus"
-	},
-	away: {
-		en: "Away mode",
-		nl: "Weg modus"
-	},
-	event: {
-		en: "Event mode",
-		nl: "Evenement modus"
-	},
-	holiday: {
-		en: "Holiday mode",
-		nl: "Vakantie modus"
-	},
-	manual: {
-		en: "Manual mode",
-		nl: "Handmatige modus"
-	},
-	movie: {
-		en: "Movie mode",
-		nl: "Film modus"
-	},
-	party: {
-		en: "Party mode",
-		nl: "Feest modus"
-	},
-	quiet: {
-		en: "Quiet mode",
-		nl: "Stilte modus"
-	},
-	relax: {
-		en: "Relax mode",
-		nl: "Ontspan modus"
-	},
-	secure: {
-		en: "Secure mode",
-		nl: "Veilige modus"
-	},
-	sleep: {
-		en: "Sleep mode",
-		nl: "Slaap modus"
-	},
-}
-
-function getDeviceTemplate( device ) {
-	var language = Homey.manager( 'i18n' ).getLanguage( );
-	return 	        {
-        name: name[device][language],
-        data: {
-            id: Device.guid(),
-        },
-        "class": "onoff",
-        capabilities: [ "onoff" ],
-    	icon: "../assets/" + device + ".svg"
-    }
-}
-
 const Device = require('../../general/drivers/device.js');
 const Mode   = require('../../general/drivers/mode.js');
 const driver = new Mode(config);
@@ -98,27 +38,44 @@ module.exports = Object.assign(
 	{ init: (devices, callback) => driver.init(devices, callback) }
 );
 
-module.exports.pair = function( other ) {
-	other.on('list_devices', function( data, callback ){
+module.exports.pair = function( socket ) {
+    socket.on('log', function( msg, callback ) {
+        console.log(msg);
+        callback( null, "ok" );
+    });
 
-		var language = Homey.manager( 'i18n' ).getLanguage( );
-		
+    socket.on('getIcons', function( data, callback ) {
+        console.log("Adding new device");
+
         var device_data = [
-	        getDeviceTemplate('mode'),
-	        getDeviceTemplate('away'),
-	        getDeviceTemplate('event'),
-	        getDeviceTemplate('holiday'),
-	        getDeviceTemplate('manual'),
-	        getDeviceTemplate('movie'),
-	        getDeviceTemplate('party'),
-	        getDeviceTemplate('quiet'),
-	        getDeviceTemplate('relax'),
-	        getDeviceTemplate('secure'),
-	        getDeviceTemplate('sleep'),
+	        getIconNameAndLocation('mode'),
+	        getIconNameAndLocation('away'),
+	        getIconNameAndLocation('event'),
+	        getIconNameAndLocation('holiday'),
+	        getIconNameAndLocation('manual'),
+	        getIconNameAndLocation('movie'),
+	        getIconNameAndLocation('party'),
+	        getIconNameAndLocation('quiet'),
+	        getIconNameAndLocation('relax'),
+	        getIconNameAndLocation('secure'),
+	        getIconNameAndLocation('sleep'),
 	    ]
 
         callback( null, device_data );
+    });
 
+    socket.on('disconnect', function(){
+        console.log("User aborted pairing, or pairing is finished");
     })
 };
 
+driver.updateRealtime = function(args, device, state) {		
+    module.exports.realtime( args, device, state);
+};
+
+function getIconNameAndLocation( name ) {
+	return {
+		"name": name,
+		"location": "../assets/" + name + ".svg"
+	}
+}
