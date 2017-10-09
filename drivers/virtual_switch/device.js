9b40b0fd28d5e7be10289dcc94eb93f1698e449a
Arjan Kranenburg <Arjan.Kranenburg@gmail.com>
2017-10-09 20:35:10 +0200

    Replaced custom alarm_state with Homey's own homealarm_state

--- drivers/virtual_switch/device.js
+++ drivers/virtual_switch/device.js
@@ -22,11 +22,6 @@ class VirtualDevice extends Homey.Device {
     let triggerDevice = new Homey.FlowCardTriggerDevice('press');
     triggerDevice.register();
 
-    let alarmStateIsTrigger = new Homey.FlowCardTriggerDevice('alarm_state_is');
-    alarmStateIsTrigger.register();
-    this.registerFlowCardRunListener(alarmStateIsTrigger);
-
-
     // When capability is changed
     this.registerMultipleCapabilityListener(this.getCapabilities(), (valueObj, optsObj) => {
       this.log(this.getName() + ' -> Capability changed: ' + JSON.stringify(valueObj));
@@ -36,8 +31,6 @@ class VirtualDevice extends Homey.Device {
 
       return Promise.resolve();
     }, 500);
-
-    this.registerSingleCapabilityListener('alarm_state', alarmStateIsTrigger);
   }
 
   // this method is called when the Device is added
@@ -49,50 +42,6 @@ class VirtualDevice extends Homey.Device {
   onDeleted() {
     this.log('device deleted: ' + this.getName());
   }
-
-  registerFlowCardRunListener(flowCardTrigger) {
-    flowCardTrigger.registerRunListener(( args, state ) => {
-      this.log('Flow card is triggered: ' + JSON.stringify(state));
-
-      let argums = cleanJson(args);
-      let firstArg = Object.keys(argums)[0]; // Should I iterate over all arguments?
-      let triggerState = argums[firstArg];
-
-      if (state === triggerState) {
-        return Promise.resolve( true );
-      } else {
-        return Promise.resolve( false );
-      }
-    })
-  }
-
-  registerSingleCapabilityListener(capability, flowCardTrigger){
-    this.registerCapabilityListener(capability, ( value, opts ) => {
-      this.log(this.getName() + ' -> ' + capability + ' changed: ' + JSON.stringify(value) );
-
-      flowCardTrigger.trigger( this, {}, value ) // Fire and forget
-        .catch( this.error );
-
-        return Promise.resolve();
-      }, 500);
-  }
 }
 
 module.exports = VirtualDevice;
-
-function cleanJson (object){
-    var simpleObject = {};
-    for (var prop in object ){
-        if (!object.hasOwnProperty(prop)){
-            continue;
-        }
-        if (typeof(object[prop]) == 'object'){
-            continue;
-        }
-        if (typeof(object[prop]) == 'function'){
-            continue;
-        }
-        simpleObject[prop] = object[prop];
-    }
-    return simpleObject; // returns cleaned up JSON
-};
